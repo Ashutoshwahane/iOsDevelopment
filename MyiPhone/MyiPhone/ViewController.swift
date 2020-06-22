2ff21159fe0a5b6fdcdae4bea686e19bf6ffd48c
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-06-22 20:11:39 +0530

    testing to run on iphone 5s

---
+++ MyiPhone/MyiPhone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyiPhone
+//
+//  Created by Ashutosh Wahane on 20/06/20.
+//  Copyright © 2020 Ashutosh Wahane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
